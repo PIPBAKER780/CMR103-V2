73fc918d6e4c7063f6266985211f0028ebe2ab31
Pip Tuninbaker <1024973@student.sae.edu.au>
2021-12-14 13:18:18 +1000

    Week 13 Update

---
+++ Assets/CanDetector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanDetector : MonoBehaviour
+{
+    public Material GreenTransparent, RedTransparent;
+    Renderer r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Renderer>();
+        r.material = RedTransparent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Can")
+        {
+            r.material = GreenTransparent;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Can")
+        {
+            r.material = RedTransparent;
+        }
+    }
+}
